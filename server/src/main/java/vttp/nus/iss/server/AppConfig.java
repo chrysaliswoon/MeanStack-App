73c6784bd9a6f9bab44ef17b1486e1b87ec08912
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2023-02-04 23:05:01 +0800

    created CORS registry

--- server/src/main/java/vttp/nus/iss/server/AppConfig.java
+++ server/src/main/java/vttp/nus/iss/server/AppConfig.java
@@ -8,14 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public WebMvcConfigurer configureCORS() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMapping(CorsRegistry registry) {
-                registry.addMapping("/api/*")
-                        .allowedOrigins("*");
-            }
-        };
-    }
+    //! Clarify this section with instructor
+
+	@Bean 
+	public WebMvcConfigurer configureCORS() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/*")
+					.allowedOrigins("*");
+			}
+		};
+	}
 }
