f25f0735ca92f192f5f6a3cd9f28b9ab499ca15e
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2023-02-04 21:09:30 +0800

    first commit

---
+++ client/src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+const components: any [] = [
+    
+]
+
+@NgModule({
+    imports: components,
+    exports: components
+})
+
+export class MaterialModule{}
