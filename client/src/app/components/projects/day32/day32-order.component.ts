effbff42eda096ae30bdb0a2acbeea7e9d248d94
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2023-02-09 00:59:35 +0800

    Created cart items

--- client/src/app/components/projects/day32/day32-order.component.ts
+++ client/src/app/components/projects/day32/day32-order.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Order } from 'src/app/models/day32';
 
 @Component({
@@ -6,9 +7,51 @@ import { Order } from 'src/app/models/day32';
   templateUrl: './day32-order.component.html',
   styleUrls: ['./day32-order.component.css']
 })
-export class Day32OrderComponent {
+export class Day32OrderComponent implements OnInit{
 
-  name = 'Alex'
+  name = ''
   order!: Order
+  form!: FormGroup
+  lineItems!: FormArray
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.form = this.createForm()
+  }
+
+  private createForm(): FormGroup {
+    this.lineItems = this.fb.array([])
+    return this.fb.group({
+      name: this.fb.control<string>('Fred', [Validators.required, Validators.minLength(10)]),
+      email:this.fb.control<string>('fed@gmail.com', [Validators.required, Validators.email]),
+      lineItems: this.lineItems
+    })
+  }
+
+  processForm(){
+    this.order = this.form.value as Order
+    this.name = this.form.controls['name'].value
+    console.info(">>> Form Data: ", this.order)
+  }
+
+  clearForm() {
+    this.form = this.createForm()
+  }
+
+  public addItem() {
+    this.lineItems.push(this.createLineItem())
+  }
+
+  private createLineItem(): FormGroup {
+    return this.fb.group({
+      item: this.fb.control<string>('apple', [Validators.required]),
+      quantity: this.fb.control<number>(1, [Validators.required, Validators.min(1), Validators.max(100)])
+    })
+  }
+
+  public deleteItem(i: number) {
+    this.lineItems.removeAt(i)
+  }
 
 }
